7eeb883fad730f2b86da4370811ba952fc9ee7ae
Zhenhua yu <zyu0628@gmail.com>
2020-11-16 20:59:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ function checkCollision(rock) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = dodgerRightEdge + 40;
+    const dodgerRightEdge = dodgerRightEdge + 40
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = rockRightEdge + 20;
+    const rockRightEdge = rockRightEdge + 20
 
     if ((rockLeftEdge<=dodgerLeftEdge&&rockRightEdge>=dodgerLeftEdge)
     || (rockLeftEdge>=dodgerLeftEdge&&rockRightEdge<=dodgerRightEdge)
